85770dcbbdbd97a888dfc8d5dfb98cc3a782989b
vjurenka <jurenka@jurenka.sk>
2014-05-02 22:58:04 +0200

    leave group unit test

--- src/PA036/SocialNetworkBundle/Tests/Service/GroupServiceTest.php
+++ src/PA036/SocialNetworkBundle/Tests/Service/GroupServiceTest.php
@@ -71,4 +71,45 @@ class GroupServiceTest extends ServiceTestCase
 
         $this->assertNotEquals(null, $groupMemberShip);
     }
+
+    public function testLeaveGroup(){
+        $entityManager = $this->entityManager;
+
+        $user1 = new User();
+        $user1->setEmail('test2@test.test');
+        $user1->setFirstName('test');
+        $user1->setLastName('test');
+        $user1->setPassword('random');
+        $user1->setLastLogin(new \DateTime(date('Y-m-d H:i:s')));
+
+        $entityManager->persist($user1);
+
+        $group1 = new Group();
+        $group1->setName('testing2 group');
+        $group1->setDescription('lorem ipsum');
+
+        $entityManager->persist($group1);
+        $entityManager->flush();
+
+        $service = new GroupService($entityManager);
+        $service->joinGroup($group1, $user1);
+
+        $this->assertNotEquals(0, $group1->getGroupId());
+        $this->assertNotEquals(0, $user1->getUserId());
+
+        $groupMemberShip = $entityManager->find('PA036\SocialNetworkBundle\Entity\UserGroupMap',
+            array('group' => $group1->getGroupId(), 'user' => $user1->getUserId()));
+
+        $this->assertNotEquals(null, $groupMemberShip);
+
+        $service->leaveGroup($group1, $user1);
+
+        $entityManager->refresh($groupMemberShip);
+        $entityManager->detach($groupMemberShip);
+
+        $groupMemberShip = $entityManager->find('PA036\SocialNetworkBundle\Entity\UserGroupMap',
+            array('group' => $group1->getGroupId(), 'user' => $user1->getUserId()));
+
+        $this->assertEquals(null, $groupMemberShip);
+    }
 }
